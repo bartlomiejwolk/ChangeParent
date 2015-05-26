b7a144aee1a5dcba02ae5abd2870648cd61dbf50
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-26 11:45:27 +0200

    Cleanup  code

--- ChangeParent.cs
+++ ChangeParent.cs
@@ -1,44 +1,33 @@
 ﻿// Copyright (c) 2015 Bartlomiej Wolk (bartlomiejwolk@gmail.com)
-//  
-// This file is part of the ChangeParent extension for Unity.
-// Licensed under the MIT license. See LICENSE file in the project root folder.
+// 
+// This file is part of the ChangeParent extension for Unity. Licensed under
+// the MIT license. See LICENSE file in the project root folder.
 
 using UnityEngine;
-using System.Collections;
 
 namespace ChangeParentEx {
 
     /// On enable, change object's parent to "Clones"
     public class ChangeParent : MonoBehaviour {
-
         #region CONSTANTS
 
-        public const string Version = "v0.1.0";
         public const string Extension = "ChangeParent";
+        public const string Version = "v0.1.0";
 
-        #endregion
-        
+        #endregion CONSTANTS
+
         #region FIELDS
 
         /// <summary>
-        /// Allows identify component in the scene file when reading it with
-        /// text editor.
+        ///     Allows identify component in the scene file when reading it with
+        ///     text editor.
         /// </summary>
         [SerializeField]
         private string componentName = "ChangeParent";
 
-        #endregion
+        #endregion FIELDS
 
         #region INSECTOR FIELDS
-        /// Select how to find a new parent.
-        [SerializeField]
-        private Options option;
-
-        [SerializeField]
-        private string parentName = "Clones";
-
-        [SerializeField]
-        private GameObject parentGO;
 
         /// Delay before changing parent.
         [SerializeField]
@@ -46,25 +35,20 @@ namespace ChangeParentEx {
 
         [SerializeField]
         private string description = "Description";
- 
-        #endregion
+
+        /// Select how to find a new parent.
+        [SerializeField]
+        private Options option;
+
+        [SerializeField]
+        private GameObject parentGO;
+
+        [SerializeField]
+        private string parentName = "Clones";
+
+        #endregion INSECTOR FIELDS
 
         #region PROPERTIES
-        /// Select how to find a new parent.
-        public Options Option {
-            get { return option; }
-            set { option = value; }
-        }
-
-        public string ParentName {
-            get { return parentName; }
-            set { parentName = value; }
-        }
-
-        public GameObject ParentGO {
-            get { return parentGO; }
-            set { parentGO = value; }
-        }
 
         /// Delay before changing parent.
         public float Delay {
@@ -73,27 +57,47 @@ namespace ChangeParentEx {
         }
 
         /// <summary>
-        /// Optional text to describe purpose of this instance of the component.
+        ///     Optional text to describe purpose of this instance of the
+        ///     component.
         /// </summary>
         public string Description {
             get { return description; }
             set { description = value; }
         }
-        #endregion
+
+        /// Select how to find a new parent.
+        public Options Option {
+            get { return option; }
+            set { option = value; }
+        }
+
+        public GameObject ParentGO {
+            get { return parentGO; }
+            set { parentGO = value; }
+        }
+
+        public string ParentName {
+            get { return parentName; }
+            set { parentName = value; }
+        }
+
+        #endregion PROPERTIES
 
         #region UNITY MESSAGES
+
         private void OnEnable() {
             switch (Option) {
                 case Options.Name:
                     Invoke("AssignParentByName", Delay);
                     break;
+
                 case Options.Transform:
                     Invoke("AssignParentByTransform", Delay);
                     break;
             }
         }
 
-        #endregion
+        #endregion UNITY MESSAGES
 
         #region METHODS
 
@@ -112,7 +116,7 @@ namespace ChangeParentEx {
             transform.parent = ParentGO.transform;
         }
 
-        #endregion
+        #endregion METHODS
     }
 
 }
--- Editor/ChangeParentEditor.cs
+++ Editor/ChangeParentEditor.cs
@@ -1,36 +1,26 @@
 ﻿// Copyright (c) 2015 Bartlomiej Wolk (bartlomiejwolk@gmail.com)
-//  
-// This file is part of the ChangeParent extension for Unity.
-// Licensed under the MIT license. See LICENSE file in the project root folder.
+// 
+// This file is part of the ChangeParent extension for Unity. Licensed under
+// the MIT license. See LICENSE file in the project root folder.
 
-using UnityEngine;
-using System.Collections;
 using UnityEditor;
+using UnityEngine;
 
 namespace ChangeParentEx {
 
     [CustomEditor(typeof (ChangeParent))]
     public class ChangeParentEditor : Editor {
-
         #region SERIALIZED PROPERTIES
 
-        private SerializedProperty option;
-        private SerializedProperty parentName;
-        private SerializedProperty parentGO;
         private SerializedProperty delay;
         private SerializedProperty description;
+        private SerializedProperty option;
+        private SerializedProperty parentGO;
+        private SerializedProperty parentName;
 
-        #endregion
+        #endregion SERIALIZED PROPERTIES
 
         #region UNITY MESSAGES
-
-        private void OnEnable() {
-            option = serializedObject.FindProperty("option");
-            parentName = serializedObject.FindProperty("parentName");
-            parentGO = serializedObject.FindProperty("parentGO");
-            delay = serializedObject.FindProperty("delay");
-            description = serializedObject.FindProperty("description");
-        }
 
         public override void OnInspectorGUI() {
             //ChangeParent script = (ChangeParent)target;
@@ -40,7 +30,7 @@ namespace ChangeParentEx {
             DrawDescriptionTextArea();
 
             EditorGUILayout.Space();
-            
+
             DrawOptionDropdown();
             HandleOptionSelection();
             DrawDelayField();
@@ -48,19 +38,17 @@ namespace ChangeParentEx {
             serializedObject.ApplyModifiedProperties();
         }
 
-        #endregion
+        private void OnEnable() {
+            option = serializedObject.FindProperty("option");
+            parentName = serializedObject.FindProperty("parentName");
+            parentGO = serializedObject.FindProperty("parentGO");
+            delay = serializedObject.FindProperty("delay");
+            description = serializedObject.FindProperty("description");
+        }
+
+        #endregion UNITY MESSAGES
 
         #region INSPECTOR CONTROLS
-        private void HandleOptionSelection() {
-            switch (option.enumValueIndex) {
-                case (int) Options.Name:
-                    EditorGUILayout.PropertyField(parentName);
-                    break;
-                case (int) Options.Transform:
-                    EditorGUILayout.PropertyField(parentGO);
-                    break;
-            }
-        }
 
         private void DrawDelayField() {
             EditorGUILayout.PropertyField(
@@ -68,6 +56,11 @@ namespace ChangeParentEx {
                 new GUIContent(
                     "Delay",
                     "Delay before changing parent."));
+        }
+
+        private void DrawDescriptionTextArea() {
+            description.stringValue = EditorGUILayout.TextArea(
+                description.stringValue);
         }
 
         private void DrawOptionDropdown() {
@@ -86,24 +79,30 @@ namespace ChangeParentEx {
                     ChangeParent.Extension));
         }
 
-        private void DrawDescriptionTextArea() {
-            description.stringValue = EditorGUILayout.TextArea(
-                description.stringValue);
+        private void HandleOptionSelection() {
+            switch (option.enumValueIndex) {
+                case (int) Options.Name:
+                    EditorGUILayout.PropertyField(parentName);
+                    break;
+
+                case (int) Options.Transform:
+                    EditorGUILayout.PropertyField(parentGO);
+                    break;
+            }
         }
- 
-        #endregion
+
+        #endregion INSPECTOR CONTROLS
 
         #region METHODS
 
         [MenuItem("Component/ChangeParent")]
         private static void AddEntryToComponentMenu() {
             if (Selection.activeGameObject != null) {
-                Selection.activeGameObject.AddComponent(typeof(ChangeParent));
+                Selection.activeGameObject.AddComponent(typeof (ChangeParent));
             }
         }
- 
-        #endregion
 
+        #endregion METHODS
     }
 
 }
