be02e3c3b5cfe0a9da3d917d7f2df0ddfa457745
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-26 10:30:01 +0200

    Initial commit

---
+++ ChangeParent.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace OneDayGame {
+
+	/// On enable, change object's parent to "Clones"
+	public class ChangeParent : GameComponent {
+
+		/// How to find a new parent options.
+		public enum Options {
+			/// Search for parent by its name.
+			Name,
+			/// Pass a transform to be a parent.
+			Transform }
+
+		/// Select how to find a new parent.
+		[SerializeField]
+		private Options _option;
+
+		[SerializeField]
+		private string _parentName = "Clones";
+
+		[SerializeField]
+		private GameObject _parentGO;
+
+		/// Delay before changing parent.
+		[SerializeField]
+		private float _delay;
+
+		public override void Start () {
+			base.Start();
+		}
+
+		public override void Update () {
+			base.Update();
+		}
+
+		public override void OnEnable() {
+			switch (_option) {
+				case Options.Name:
+					// Find parent go by name.
+					_parentGO = GameObject.Find(_parentName);
+					// Create parent if doesn't exists.
+					if (_parentGO == null) {
+						_parentGO = new GameObject(_parentName);
+					}
+					AssignNewParent();
+					break;
+				case Options.Transform:
+					AssignNewParent();
+					break;
+			}
+		}
+
+		private void AssignNewParent() {
+			// Start coroutine only if the parenting should be delayed.
+			if (_delay != 0) {
+				StartCoroutine(Timer.Start(
+							_delay,
+							() => { transform.parent = _parentGO.transform; }
+							));
+			}
+			else {
+				transform.parent = _parentGO.transform;
+			}
+		}
+	}
+}
---
+++ Editor/ChangeParentEditor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using OneDayGame;
+
+[CustomEditor(typeof(ChangeParent))]
+public class ChangeParentEditor: GameComponentEditor {
+
+	private SerializedProperty _option;
+	private SerializedProperty _parentName;
+	private SerializedProperty _parentGO;
+	private SerializedProperty _delay;
+
+	public override void OnEnable() {
+		base.OnEnable();
+
+		_option = serializedObject.FindProperty("_option");
+		_parentName = serializedObject.FindProperty("_parentName");
+		_parentGO = serializedObject.FindProperty("_parentGO");
+		_delay = serializedObject.FindProperty("_delay");
+	}
+
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI();
+		//ChangeParent script = (ChangeParent)target;
+		serializedObject.Update();
+
+		EditorGUILayout.PropertyField(_option);
+		switch (_option.enumValueIndex) {
+			case (int)ChangeParent.Options.Name:
+				EditorGUILayout.PropertyField(_parentName);
+				break;
+			case (int)ChangeParent.Options.Transform:
+				EditorGUILayout.PropertyField(_parentGO);
+				break;
+		}
+		EditorGUILayout.PropertyField(_delay);
+
+		serializedObject.ApplyModifiedProperties();
+		// Save changes
+		/*if (GUI.changed) {
+			EditorUtility.SetDirty(script);
+		}*/
+	}
+}
